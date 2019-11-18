46b8d10755a83558ac13e2700a2e0627c5c64602
arijit248 <Arijit Kundu>
2019-11-18 16:26:16 +0530

    18-11-2019

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Main from './Container/Main/Main';
+
+const App = () => {
+  return (
+    <div className="App Bg">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
